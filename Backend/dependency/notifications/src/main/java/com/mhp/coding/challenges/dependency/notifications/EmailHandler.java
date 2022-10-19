0f31fb2bae9e9228b82a4476c0592f88c82573f2
Kerem Ali Topal <keremalitopal@gmail.com>
2022-10-19 14:15:56 +0200

    implemented inquiry creation event listener actions

--- Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/EmailHandler.java
+++ Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/EmailHandler.java
@@ -1,14 +1,22 @@
 package com.mhp.coding.challenges.dependency.notifications;
 
 import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import com.mhp.coding.challenges.dependency.inquiry.InquiryCreationEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EmailHandler {
+public class EmailHandler implements ApplicationListener<InquiryCreationEvent> {
 
     private static final Logger LOG = LoggerFactory.getLogger(EmailHandler.class);
+
+    @Override
+    public void onApplicationEvent(InquiryCreationEvent event) {
+        LOG.info("Received spring custom event - " + event.getInquiry());
+        this.sendEmail(event.getInquiry());
+    }
 
     public void sendEmail(final Inquiry inquiry) {
         LOG.info("Sending email for: {}", inquiry);
--- Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/PushNotificationHandler.java
+++ Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/PushNotificationHandler.java
@@ -1,16 +1,25 @@
 package com.mhp.coding.challenges.dependency.notifications;
 
 import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import com.mhp.coding.challenges.dependency.inquiry.InquiryCreationEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PushNotificationHandler {
+public class PushNotificationHandler implements ApplicationListener<InquiryCreationEvent> {
 
     private static final Logger LOG = LoggerFactory.getLogger(PushNotificationHandler.class);
+
+    @Override
+    public void onApplicationEvent(InquiryCreationEvent event) {
+        LOG.info("Received spring custom event - " + event.getInquiry());
+        this.sendNotification(event.getInquiry());
+    }
 
     public void sendNotification(final Inquiry inquiry) {
         LOG.info("Sending push notification for: {}", inquiry);
     }
+
 }
